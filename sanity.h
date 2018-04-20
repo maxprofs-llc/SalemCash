b56ee2d990df00a2a922b16b812a91b7b44fc82f
Pastor Ombura <36679951+PastorOmbura@users.noreply.github.com>
2018-04-20 21:02:02 +0300

    Add files via upload

---
+++ sanity.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2018 The SalemCash developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SALEMCASH_COMPAT_SANITY_H
+#define SALEMCASH_COMPAT_SANITY_H
+
+bool glibc_sanity_test();
+bool glibcxx_sanity_test();
+
+#endif // SALEMCASH_COMPAT_SANITY_H
