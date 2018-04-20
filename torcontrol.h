d47d47954feaa875329c438dcdb3d46dc92fef90
Pastor Ombura <36679951+PastorOmbura@users.noreply.github.com>
2018-04-20 22:51:46 +0300

    Add files via upload

---
+++ torcontrol.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2018 The SalemCash developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/**
+ * Functionality for communicating with Tor.
+ */
+#ifndef SALEMCASH_TORCONTROL_H
+#define SALEMCASH_TORCONTROL_H
+
+#include <scheduler.h>
+
+extern const std::string DEFAULT_TOR_CONTROL;
+static const bool DEFAULT_LISTEN_ONION = true;
+
+void StartTorControl();
+void InterruptTorControl();
+void StopTorControl();
+
+#endif /* SALEMCASH_TORCONTROL_H */
