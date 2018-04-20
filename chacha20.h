ff47661a5409564ac570efa03f5e20a2bb593532
Pastor Ombura <36679951+PastorOmbura@users.noreply.github.com>
2018-04-20 21:08:56 +0300

    Add files via upload

---
+++ chacha20.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2018 The SalemCash developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SALEMCASH_CRYPTO_CHACHA20_H
+#define SALEMCASH_CRYPTO_CHACHA20_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/** A PRNG class for ChaCha20. */
+class ChaCha20
+{
+private:
+    uint32_t input[16];
+
+public:
+    ChaCha20();
+    ChaCha20(const unsigned char* key, size_t keylen);
+    void SetKey(const unsigned char* key, size_t keylen);
+    void SetIV(uint64_t iv);
+    void Seek(uint64_t pos);
+    void Output(unsigned char* output, size_t bytes);
+};
+
+#endif // SALEMCASH_CRYPTO_CHACHA20_H
